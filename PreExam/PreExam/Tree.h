00ba95fc96c793ce5d5c4206d0ce21b244ee2492
Vinskky <naitsabesdv@gmail.com>
2015-04-27 13:14:37 +0100

    tree changes

--- PreExam/PreExam/Tree.h
+++ PreExam/PreExam/Tree.h
@@ -27,9 +27,7 @@ public:
 	void Add(const TYPE& value)
 	{
 		Node<TYPE>* nou = new Node<TYPE>(value);
-
 		rootNode->son.add(nou);
-
 		nou->dad = rootNode;
 	}
 
@@ -40,21 +38,48 @@ public:
 		nou->dad = parent;
 	}
 
-	void VisitAllNodes(sList <TYPE>* list)const
+	void VisitAllNodes(sList <TYPE>* list, int option)const
 	{
-		rootNode VisitAll(list);
+
+		switch (option)
+		{
+		case 1: { rootNode PreOrderRecursive(list); break; }
+		case 2: { rootNode InOrderRecursive(list); break; }
+		case 3: { rootNode PostOrderRecursive(list); break; }
+		case 4: { rootNode PreOrderIterative(list, parent); break; }
+		case 5: { rootNode InOrderIterative(list, parent); break; }
+		case 6: { rootNode PostOrderIterative(list, parent); break; }
+		}
+		
 	}
 
-	void VisitAll(sList <TYPE>* list)
+	void PreOrderRecursive(sList <TYPE>* list)
 	{
 		list.Add(data);
 		for (int i = 0; i < list->count; i++)
-			node->VisitAll(list);
+			node->VisitAll(list, 1);
+	}
+
+	void InOrderRecursive(sList <TYPE>* list)
+	{
+		for (int i = 0; i < list->count; i++)
+		{
+			list.Add(data);
+
+		}
+		
+	}
+
+	void PostOrderRecursive(sList <TYPE>* list)
+	{
+		node->VisitAll(list, 3);
+		for (int i = 0; i < list->count; i++)
+			list.Add(data);
 	}
 
 	//visit all with iterative functions
 
-	void VisitAllPreOrder(sList <TYPE>* list, Node<TYPE>* parent)
+	void PreOrderIterative(sList <TYPE>* list, Node<TYPE>* parent)
 	{
 		list->Add(rootNode);
 			for (int i = 0; i < list->count; i++)
@@ -70,6 +95,20 @@ public:
 
 	}
 
+	void InOrderIterative(sList <TYPE>* list, Node<TYPE>* parent)
+	{
+
+	}
+
+	void lPostOrderIterative(sList<TYPE>* list, Node<TYPE> parent)
+	{
+
+	}
+
+	void Clear()
+	{
+
+	}
 };
 
 
