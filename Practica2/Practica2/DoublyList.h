33059392814a605840626eb77843ae0e11b7a58d
Vinskky <naitsabesdv@gmail.com>
2015-04-27 18:43:55 +0100

    tree

---
+++ Practica2/Practica2/DoublyList.h
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+#ifndef _DoublyList_H_
+#define _DoublyList_H_
+
+template <class tdata>
+struct  node
+{
+	tdata data;
+	node<tdata>* next;
+	node<tdata>* prev;
+	inline node(const tdata& _data)
+	{
+		data = _data;
+		next = prev = NULL;
+	}
+
+	~node(){}
+};
+
+template <class tdata>
+class DoublyList
+{
+private:
+
+	unsigned int size;
+
+public:
+
+	node<tdata>* start;
+	node<tdata>* end;
+
+	//constructors
+	inline DoublyList()
+	{
+		size = 0;
+		start = end = NULL;
+	}
+
+	~DoublyList()
+	{
+		Clear();
+	}
+
+	unsigned int Add(const tdata& item)
+	{
+		node<tdata>* = data_item;
+		data_item = new node < tdata >(item);
+
+		if (start == NULL)
+		{
+			start = end = data_item;
+		}
+		else
+		{
+			data_item->prev = end;
+			end->next = data_item;
+			end = data_item;
+		}
+
+		return(++size);
+	}
+
+	/**
+	* Deletes an item from the list
+	*/
+	bool del(node<tdata>* item)
+	{
+		if (item == NULL)
+		{
+			return (false);
+		}
+
+		// Now reconstruct the list
+		if (item->prev != NULL)
+		{
+			item->prev->next = item->next;
+
+			if (item->next != NULL)
+			{
+				item->next->prev = item->prev;
+			}
+			else
+			{
+				end = item->prev;
+			}
+		}
+		else
+		{
+			if (item->next)
+			{
+				item->next->prev = NULL;
+				start = item->next;
+			}
+			else
+			{
+				start = end = NULL;
+			}
+		}
+
+		delete item;
+		--size;
+		return(true);
+	}
+
+	/*
+	const unsigned int delNodes(unsigned int position, unsigned int numNodes)
+	{
+		if (start != NULL)
+		{
+
+		}
+	}
+
+	*/
+	int Count()
+	{
+		return size;
+	}
+
+	void Clear()
+	{
+		node<tdata>*   p_data;
+		node<tdata>*   p_next;
+		p_data = start;
+
+		while (p_data != NULL)
+		{
+			p_next = p_data->next;
+			delete p_data;
+			p_data = p_next;
+		}
+
+		start = end = NULL;
+		size = 0;
+	}
+
+};
+
+#endif _DoublyList_H_
--- Practica2/Practica2/Tree.h
+++ Practica2/Practica2/Tree.h
@@ -1,22 +1,137 @@
 #ifndef _TREE_H_
 #define _TREE_H_
 
-struct node
+#include "DoublyList.h"
+#include "Stack.h"
+
+template <class TYPE>
+struct NodeTree
 {
-	char data;
-	node* left;
-	node* right;
+	TYPE data;
+	NodeTree*dad;
+	DoublyList son;
 };
 
+template <class TYPE>
 class Tree
 {
 private:
-	node* root;
-public: 
-	Tree(){}
-	Tree(node* _root, char _data){ root = _root; data = _data; }
-	~Tree();
+	NodeTree* rootNode;
+public:
+
+	Tree(TYPE value)
+	{
+		NodeTree<TYPE>* nou = new NodeTree<TYPE>;
+		nou->data = value;
+	}
+
+	void Add(const TYPE& value)
+	{
+		NodeTree<TYPE>* nou = new NodeTree<TYPE>(value);
+		rootNode->son.add(nou);
+	}
+
+	void Add(const TYPE& value, const NodeTree* parent)
+	{
+		NodeTree<TYPE>* nou = new NodeTree<TYPE>(value);
+		parent->son.Add(nou);
+		nou->dad = parent;
+	}
+
+	void VisitAllNodes(DoublyList <TYPE>* list, int option)const
+	{
+
+		switch (option)
+		{
+		case 1: { rootNode PreOrderRecursive(list); break; }
+		case 2: { rootNode InOrderRecursive(list); break; }
+		case 3: { rootNode PostOrderRecursive(list); break; }
+		case 4: { rootNode PreOrderIterative(list, parent); break; }
+		case 5: { rootNode InOrderIterative(list, parent); break; }
+		case 6: { rootNode PostOrderIterative(list, parent); break; }
+		}
+
+	}
+
+	void PreOrderRecursive(DoublyList <TYPE>* list)
+	{
+		list.Add(data);
+		for (int i = 0; i < list->count; i++)
+			rootNode->data->VisitAllNodes(list, 1);
+	}
+
+	void InOrderRecursive(DoublyList <TYPE>* list)
+	{
+		unsigned int medium = rootNode->son.count() / 2;
+		
+		for (unsigned int i = 0; i < medium; i++)
+		{
+			rootNode->data->VisitAllNodes(list, 2);
+		}
 
 
+		for (int i = 0; i < list->count; i++)
+			rootNode->data->VisitAllNodes(list, 2);
+
+	}
+
+	void PostOrderRecursive(DoublyList <TYPE>* list)
+	{
+		
+		for (int i = 0; i < list->count; i++)
+			rootNode->data->VisitAllNodes(list, 3);
+
+		list.Add(data);
+	}
+
+	//visit all with iterative functions
+
+	void PreOrderIterative(DoublyList <TYPE>* list, NodeTree<TYPE>* parent)
+	{
+		p2Stack<TYPE>* stack;
+		NodeTree<TYPE>* tmp;
+
+		for (int i = 0; i < list->count; i++)
+		{
+			if (parent->son->start != NULL )
+			{
+				stack->Push(parent->data);
+				tmp->data = list->start->data;
+			}
+			else 
+			{
+				if (list->start->next != NULL)
+				{
+					parent->data = list->start->next->data;
+					stack->Push(parent->data);
+					tmp->data = parent->data;
+				}
+			}
+			stack->Pop(parent->data);
+			parent->data = tmp->data;
+		}
+	}
+
+	void InOrderIterative(DoublyList <TYPE>* list, NodeTree<TYPE>* parent)
+	{
+
+	}
+
+	void PostOrderIterative(DoublyList<TYPE>* list, NodeTree<TYPE> parent)
+	{
+		p2Stack<TYPE>* stack;
+		NodeTree<TYPE>* tmp;
+
+		for (int i = 0; )
+		
+	}
+
+	void Clear()
+	{
+
+	}
 };
+
+
+
 #endif _TREE_H_
