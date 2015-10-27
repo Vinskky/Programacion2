ed80466f4344463b5a91e651f60ce9b1333b7db6
Vinskky <naitsabesdv@gmail.com>
2015-10-27 13:53:45 +0100

    constructor String

--- String/String/String.cpp
+++ String/String/String.cpp
@@ -1,20 +1,67 @@
 #include <iostream>
 #include "String.h"
 
-String::String(){}
+String::String() : str(NULL), capacity(NULL){}
 
 String::String(String& str)
 {
-	
-	
+	capacity = str.capacity;
+	str = new char[capacity];	
 }
 
 String::String(char* str)
 {
-	
+	char* tmp;
+	*tmp = *str;
+	while (tmp != '\0')
+	{
+		capacity++;
+		tmp++;
+	}
 }
-String::String(const uint capacity){}
+String::String(uint cap): capacity(cap)
+{
+	str = new char[capacity];
+}
 
-String::~String(){};
+String::~String()
+{
+	delete str;
+	capacity = 0;
+};
+bool String::operator == (const String& a)
+{
+	if (str == a.str)
+	{
+		return true;
+	}
+	return false;
+}
 
+bool String::operator == (char* a)
+{
+	if (a == NULL)
+		return false;
+	char* tmp, *tmp2;
+	*tmp = *a;
+	*tmp2 = *str;
+	while(tmp == tmp2)
+	{
+		tmp++;
+		tmp2++;
+		if (*tmp == '\0' && *tmp2 == '\0')
+			return true;
+	}
+	return false;
+}
+
+bool String::operator += (String& a)
+{
+
+}
+
+bool String::operator += (char* a)
+{
+
+}
 const char* String::C_str(){ return str; };
--- String/String/String.h
+++ String/String/String.h
@@ -1,6 +1,6 @@
 #ifndef _String_H_
 #define _String_H_
-
+#define ALLOC 100
 typedef unsigned int uint;
 
 class String
@@ -9,6 +9,11 @@ private:
 	char* str;
 	uint capacity;
 
+	void Realloc()
+	{
+		capacity += ALLOC;
+		str[capacity];
+	}
 public:
 	String(){};
 	String(String& str){}
@@ -17,6 +22,25 @@ public:
 
 	~String(){};
 
+	bool operator == (const String& a)
+	{
+
+	}
+
+	bool operator == (char* a)
+	{
+
+	}
+
+	bool operator += (String& a)
+	{
+
+	}
+
+	bool operator += (char* a)
+	{
+
+	}
 	const char* C_str(){}
 
 };
